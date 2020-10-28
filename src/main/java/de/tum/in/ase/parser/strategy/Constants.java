ac8709549efdfb1f802b48e82abaeb74ac104e56
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-10-28 21:12:54 +0100

    Whitespace

--- src/main/java/de/tum/in/ase/parser/strategy/Constants.java
+++ src/main/java/de/tum/in/ase/parser/strategy/Constants.java
@@ -3,7 +3,7 @@ package de.tum.in.ase.parser.strategy;
 public class Constants {
 
     // Category for grouping of rules, which detect code duplications
-    public  static final String CPD_CATEGORY = "Copy/Paste Detection";
+    public static final String CPD_CATEGORY = "Copy/Paste Detection";
 
     private Constants() {
     }
