89b5a0839e2728a0a4160cdebbb9d7eab589582a
Alexander Stephan <alexander.stephan@tum.de>
2021-09-06 11:35:02 +0200

    Refactor code based on suggestions

--- src/main/java/de/tum/in/ase/parser/strategy/GCCParser.java
+++ src/main/java/de/tum/in/ase/parser/strategy/GCCParser.java
@@ -15,36 +15,62 @@ import de.tum.in.ase.parser.domain.Report;
 
 public class GCCParser implements ParserStrategy {
 
+    // The message is split into two segments
+    private static final int SEGMENTS_COUNT = 2;
+
+    // Segment 0 (Header): Contains info about filename, row, column, type etc.
+    private static final int HEADER_SEGMENT_POS = 0;
+
+    // Segment 1 (Body): Body contains the trace of the error (possibly rendered with ASCII art)
+    private static final int BODY_SEGMENT_POS = 1;
+
     // Locations in regex group
-    protected static final int NO_SEGS = 3;
+    private static final int FILE_POS = 1;
 
-    protected static final int FILE_POS = 1;
+    private static final int ROW_POS = 2;
 
-    protected static final int ROW_POS = 2;
+    private static final int COLUMN_POS = 3;
 
-    protected static final int COL_POS = 3;
+    private static final int TYPE_POS = 4;
 
-    protected static final int TYPE_POS = 4;
+    private static final int DESCRIPTION_POS = 5;
 
-    protected static final int DESC_POS = 5;
-
-    protected static final int ERROR_POS = 6;
-
-    protected static final int BODY_SEG = 2;
-
-    // Categories
-    protected static final String MEMORY = "Memory";
-
-    protected static final String BAD_PRACTICE = "BadPractice";
-
-    protected static final String SECURITY = "Security";
-
-    protected static final String UNDEFINED_BEHAVIOR = "UndefinedBehavior";
-
-    protected static final String MISC = "Misc"; // For various other results, that are not part of the static analysis
+    private static final int ERROR_POS = 6;
 
     // Map that contains the matching category for each error
-    protected static final Map<String, String> categories = new HashMap<>();
+    private static final Map<String, String> categories = new HashMap<>();
+
+    private static final String ANALYZER_PREFIX = "[-Wanalyzer";
+
+    // Categories
+    private static final String MEMORY = "Memory";
+
+    private static final String BAD_PRACTICE = "BadPractice";
+
+    private static final String SECURITY = "Security";
+
+    private static final String UNDEFINED_BEHAVIOR = "UndefinedBehavior";
+
+    private static final String MISC = "Misc"; // For various other results that are not part of the static analysis
+
+    // Used to parse the first line of an issue which contains all the essential data
+    // e.g. "ascii_table.c:7:13: warning: variable ‘arr’ set but not used [-Wunused-but-set-variable]".
+    // A colon ":" is the separator symbol from GCC.
+    private static final String HEADER_REGEX = "([^:^\\n]+):(\\d+):(\\d+):\\s(\\w+\\s*\\w*):\\s(.+)(\\[.+])";
+    //                                           ^          ^      ^      ^                 ^      ^
+    //                                           |          |      |      |                 |      |
+    //                                           |          |      |      |                 |      |
+    //                                           |          |      |      |                 |       +- error name eg. [-Wunused-but-set-variable]
+    //                                           |          |      |      |                 +- message text e.g. warning: variable ‘arr’ set but not used
+    //                                           |          |      |      +- type e.g. (error|warning|note)
+    //                                           |          |      +- column e.g. 13
+    //                                           |          +- row e.g. 7
+    //                                           +- filename e.g. ascii_table.c
+
+    // More generic regex similar to HEADER_REGEX, that describes the beginning of a basic GCC message, that is used to divide the output into chunks.
+    // A look ahead regex (see "?=") is used, since we need to keep the delimiter after the split.
+    // We need to include new line, so we can guarantee that we actually match to a new message.
+    private static final String DELIM_REGEX = "(?=(\\n([^:^\\n]+):(\\d)+:(\\d)+:(.)+:(.)+))";
 
     @Override
     public Report parse(Document doc) {
@@ -56,69 +82,77 @@ public class GCCParser implements ParserStrategy {
     /**
      * Constructs issues and adds them to the static analysis report.
      *
-     * @param doc Contains the output from GCC SCA
-     * @param report The report the issues will be added to
+     * @param doc Contains the output from GCC SCA.
+     * @param report The report the issues will be added to.
      */
     private void extractIssues(Document doc, Report report) {
-        initCategoryMapping();
-        List<Issue> issues = new ArrayList<>();
         Element gccLog = doc.getDocumentElement();
 
-        // Splits up entries by the following format: <filename>:<row>:<col>: <description>
-        // ?= is a look ahead regex, so we can keep the delimiter after the split
-        String delim = "(?=(\\n(.)+.\\w:(\\d)+:(\\d)+:(.)+:(.)+))";
-        String[] results = gccLog.getTextContent().split(delim);
+        String[] logItems = gccLog.getTextContent().split(DELIM_REGEX);
 
-        // For simplicity, we use \n also as the delimiter, so we only get one actual match for each entry
-        String infoEntryRegex = "([^:^\\n]+):(\\d+):(\\d+):\\s(\\w+\\s*\\w*):\\s(.+)(\\[.+\\])\\n";
-        Pattern errorDetails = Pattern.compile(infoEntryRegex);
-        Matcher m = errorDetails.matcher("");
+        Pattern pattern = Pattern.compile(HEADER_REGEX);
+        Matcher matcher = pattern.matcher("");
 
-        for (String entry : results) {
+        initCategoryMapping();
+
+        List<Issue> issues = new ArrayList<>();
+
+        for (String entry : logItems) {
+
+            // Chops off the leading \n which makes parsing more clear, as we avoid an empty extra segment
+            String[] segments = entry.substring(1).split("\n", SEGMENTS_COUNT);
+
+            if (segments.length < 2) {
+                continue;
+            }
+
+            String header = segments[HEADER_SEGMENT_POS];
+            String body = segments[BODY_SEGMENT_POS];
+
+            matcher.reset(header);
+
+            if (!matcher.find()) {
+                continue;
+            }
+
+            // Construct issue details based on regex groups
+            String filename = matcher.group(FILE_POS).trim();
+            Integer row = Integer.parseInt(matcher.group(ROW_POS));
+            Integer col = Integer.parseInt(matcher.group(COLUMN_POS));
+            String type = matcher.group(TYPE_POS);
+            String description = matcher.group(DESCRIPTION_POS);
+            String warningName = matcher.group(ERROR_POS);
+
+            // Only output warnings that have a name associated with it
+            if (warningName == null) {
+                continue;
+            }
+            // warningName is included in the description, as it will not be shown be Artemis otherwise
+            String message = warningName + ": " + description + "\n" + body;
+
             Issue issue = new Issue(null);
 
-            m.reset(entry);
+            issue.setMessage(message);
+            issue.setFilePath(filename);
+            issue.setStartLine(row);
+            issue.setEndLine(row);
+            issue.setStartColumn(col);
+            issue.setEndColumn(col);
+            issue.setRule(warningName);
+            issue.setPriority(type); // Could potentially be used for sorting at some point, not displayed by Artemis
 
-            // Extract attributes
-            if (m.find()) {
-                boolean isAnalyzerIssue;
-                String filename = m.group(FILE_POS).trim();
-                Integer row = Integer.parseInt(m.group(ROW_POS));
-                Integer col = Integer.parseInt(m.group(COL_POS));
-                String type = m.group(TYPE_POS);
-                String description = m.group(DESC_POS);
-                String errorName = m.group(ERROR_POS);
-                String body;
+            boolean isAnalyzerIssue = warningName.startsWith(ANALYZER_PREFIX);
 
-                // Extract output after info line
-                if (errorName != null) {
-                    String[] elements = entry.split("\n", NO_SEGS);
-                    body = elements[BODY_SEG]; // Body contains additional debug info, including ASCII art
-                    issue.setMessage(errorName + ": " + description + "\n" + body);
-                    isAnalyzerIssue = errorName.startsWith("[-Wanalyzer");
-                }
-                else {
-                    continue; // Only output errors that have a name associated with it
-                }
-
-                // Set correct category, only real static analysis issues are categorized, see https://gcc.gnu.org/onlinedocs/gcc-11.1.0/gcc/Static-Analyzer-Options.html
-                if (isAnalyzerIssue) {
-                    String category = categories.get(errorName);
-                    issue.setCategory(category);
-                }
-                else {
-                    issue.setCategory(MISC);
-                }
-
-                issue.setFilePath(filename);
-                issue.setStartLine(row);
-                issue.setEndLine(row);
-                issue.setStartColumn(col);
-                issue.setEndColumn(col);
-                issue.setRule(errorName);
-                issue.setPriority(type); // Could potentially be used for sorting at some point
-                issues.add(issue);
+            // Set correct category, only real static analysis issues are categorized, see https://gcc.gnu.org/onlinedocs/gcc-11.1.0/gcc/Static-Analyzer-Options.html
+            if (isAnalyzerIssue) {
+                String category = categories.get(warningName);
+                issue.setCategory(category);
             }
+            else {
+                issue.setCategory(MISC);
+            }
+
+            issues.add(issue);
         }
         report.setIssues(issues);
     }
